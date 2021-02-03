3a223afaef2e27bc982b07f52d32375ccee47fc6
diegotco <diegotco@yahoo.com>
2021-02-03 21:43:55 +0000

    Bastian's change the instantation outside the while loop. This fixed the problem

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-/* We are working in an If statment
+/* We are working with an If statment.
 1. All salespeople are gonna be paid 1000 dollars a week
 2. Salespeople who exceed 10 sales within a week, will get an additional bonus of 250 dollars
 */ 
@@ -11,25 +11,27 @@ class Main {
     // Initialize the known values
     int salary = 1000;
     int bonus = 250;
+    int quota = 10;
 
-String flag = "yes";
-while (flag == "yes"){
+    int flag = 1;
+while (flag == 1){
     //Get values for the unknow
-    System.out.println("How many sales did the employee this week");
+    System.out.println("How many sales did the employee this week?");
     Scanner scanner = new Scanner(System.in);
     int sales = scanner.nextInt();
-   
+    
 
     //Quick detour for the bonus earners
-    if(sales >= 10){
+    if(sales >= quota){
     salary = salary + bonus;
     }
 
 
     //Output
     System.out.println("The employee's pay is $" + salary);
-    System.out.println("Do you want to continue? Write yes or no");
-    flag = scanner.next();
+    System.out.println("Do you want to continue? Write 1 or 0");
+
+    flag = scanner.nextInt();
     scanner.close();
   }
 
